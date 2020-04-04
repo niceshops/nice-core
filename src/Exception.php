235b991c973e8e8b9c1a6f64e4b924bd47d3aba0
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-04 21:55:41 +0200

    Exception

---
+++ src/Exception.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @see       https://github.com/niceshops-com/CoreComponents for the canonical source repository
+ * @license   https://github.com/niceshops-com/CoreComponents/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsCom\CoreComponents;
+
+/**
+ * Class Exception
+ * @package NiceshopsCom\CoreComponents
+ */
+class Exception extends \Exception
+{
+    const BASIC_EXCEPTION_CODE = 1;
+    const BADFUNCTIONCALL_EXCEPTION_CODE = 2;
+    const DOMAIN_EXCEPTION_CODE = 3;
+    const INVALIDARGUMENT_EXCEPTION_CODE = 4;
+    const LENGTH_EXCEPTION_CODE = 5;
+    const LOGIC_EXCEPTION_CODE = 6;
+    const OUTOFBOUNDS_EXCEPTION_CODE = 7;
+    const OUTOFRANGE_EXCEPTION_CODE = 8;
+    const OVERFLOW_EXCEPTION_CODE = 9;
+    const RANGE_EXCEPTION_CODE = 10;
+    const RUNTIME_EXCEPTION_CODE = 11;
+    const UNDERFLOW_EXCEPTION_CODE = 12;
+    const UNEXPECTEDVALUE_EXCEPTION_CODE = 13;
+    const BADMETHODCALL_EXCEPTION_CODE = 14;
+    const API_EXCEPTION_CODE = 15;
+    const LOCK_EXCEPTION_CODE = 16;
+    const DATABASE_EXCEPTION_CODE = 17;
+    const FILE_EXISTS_EXCEPTION_CODE = 18;
+    
+    
+    const METHOD_NOT_FOUND = 404;
+    
+    
+    /**
+     * @param string $message
+     *
+     * @return Exception
+     */
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+    
+    
+    /**
+     * @param string $appendMessage
+     * @param string $separator
+     *
+     * @return Exception
+     */
+    public function appendToMessage(string $appendMessage, string $separator = " "): self
+    {
+        if (strlen($this->message) < 1) {
+            $separator = "";
+        }
+        $this->message .= $separator . $appendMessage;
+        return $this;
+    }
+    
+    
+    /**
+     * @param string $prependMessage
+     * @param string $separator
+     *
+     * @return Exception
+     */
+    public function prependToMessage(string $prependMessage, string $separator = " "): self
+    {
+        if (strlen($this->message) < 1) {
+            $separator = "";
+        }
+        $this->message = $prependMessage . $separator . $this->message;
+        return $this;
+    }
+}
---
+++ test/ExceptionTest.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @see       https://github.com/niceshops-com/CoreComponents for the canonical source repository
+ * @license   https://github.com/niceshops-com/CoreComponents/blob/master/LICENSE BSD 3-Clause License
+ */
+
+namespace NiceshopsCom\CoreComponents;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ExceptionTest
+ * @package Core
+ */
+class ExceptionTest extends TestCase
+{
+    
+    
+    /**
+     * @var Exception|MockObject
+     */
+    protected $object;
+    
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     */
+    protected function setUp()
+    {
+        $this->object = $this->getMockBuilder(Exception::class)->disableOriginalConstructor()->getMockForAbstractClass();
+    }
+    
+    
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsCom\CoreComponents\Exception::setMessage()
+     * @uses   \NiceshopsCom\CoreComponents\Exception::getMessage()
+     */
+    public function testSetMessage()
+    {
+        $message = "foo bar baz";
+        
+        $this->assertSame("", $this->object->getMessage());
+        
+        $this->object->setMessage($message);
+        $this->assertSame($message, $this->object->getMessage());
+        
+        try {
+            throw $this->object;
+        } catch (Exception $e) {
+            $this->assertSame($message, $e->getMessage());
+        }
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsCom\CoreComponents\Exception::appendToMessage()
+     * @uses   \NiceshopsCom\CoreComponents\Exception::getMessage()
+     */
+    public function testAppendToMessage()
+    {
+        $this->assertSame("", $this->object->getMessage());
+        
+        $this->object->appendToMessage("foo");
+        $this->assertSame("foo", $this->object->getMessage());
+        
+        $this->object->appendToMessage("bar");
+        $this->assertSame("foo bar", $this->object->getMessage());
+        
+        $this->object->appendToMessage("baz", " - ");
+        $this->assertSame("foo bar - baz", $this->object->getMessage());
+    }
+    
+    
+    /**
+     * @group  unit
+     * @small
+     *
+     * @covers \NiceshopsCom\CoreComponents\Exception::prependToMessage()
+     * @uses   \NiceshopsCom\CoreComponents\Exception::getMessage()
+     */
+    public function testPrependToMessage()
+    {
+        $this->assertSame("", $this->object->getMessage());
+        
+        $this->object->prependToMessage("foo");
+        $this->assertSame("foo", $this->object->getMessage());
+        
+        $this->object->prependToMessage("bar");
+        $this->assertSame("bar foo", $this->object->getMessage());
+        
+        $this->object->prependToMessage("baz", " - ");
+        $this->assertSame("baz - bar foo", $this->object->getMessage());
+    }
+    
+}
