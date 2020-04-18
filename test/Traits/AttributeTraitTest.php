5bd036847d31881467b1661062ec56414bb596c2
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-18 12:52:22 +0200

    Git Account Renaming from "niceshops-dev" to "niceshops"

--- test/Traits/AttributeTraitTest.php
+++ test/Traits/AttributeTraitTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @see       https://github.com/niceshops-dev/NiceCore for the canonical source repository
- * @license   https://github.com/niceshops-dev/NiceCore/blob/master/LICENSE BSD 3-Clause License
+ * @see       https://github.com/niceshops/NiceCore for the canonical source repository
+ * @license   https://github.com/niceshops/NiceCore/blob/master/LICENSE BSD 3-Clause License
  */
 
 namespace NiceshopsDev\NiceCore\Traits;
