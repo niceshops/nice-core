0d94b3d57ae38ca385dca1a34dc12fd6d14fee4a
Andreas Maurer <andreas.maurer@niceshops.com>
2020-04-20 08:40:28 +0200

    GitHub Repo Link Korrektur

--- src/Exception.php
+++ src/Exception.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 /**
- * @see       https://github.com/niceshops/NiceCore for the canonical source repository
- * @license   https://github.com/niceshops/NiceCore/blob/master/LICENSE BSD 3-Clause License
+ * @see       https://github.com/niceshops/nice-core for the canonical source repository
+ * @license   https://github.com/niceshops/nice-core/blob/master/LICENSE BSD 3-Clause License
  */
 
 namespace NiceshopsDev\NiceCore;
--- src/OptionAwareInterface.php
+++ src/OptionAwareInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @see       https://github.com/niceshops/NiceCore for the canonical source repository
- * @license   https://github.com/niceshops/NiceCore/blob/master/LICENSE BSD 3-Clause License
+ * @see       https://github.com/niceshops/nice-core for the canonical source repository
+ * @license   https://github.com/niceshops/nice-core/blob/master/LICENSE BSD 3-Clause License
  */
 
 namespace NiceshopsDev\NiceCore;
